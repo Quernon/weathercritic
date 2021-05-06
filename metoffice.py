a626548ba0102a75ca0b37bb245a2de0b539e133
Quernon <36922275+Quernon@users.noreply.github.com>
2021-05-06 15:41:11 +0100

    Update metoffice.py
    Created scraping function. Script now creates dict for 'pop' and 'feels-like'.

--- metoffice.py
+++ metoffice.py
@@ -4,23 +4,34 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 
+# Scrape met office data and save probability of precipiation forecast to dictionary
 today = datetime.date.today()
 tomorrow = today + datetime.timedelta(days=1)
 
+# Location key corresponding to Manchester
 location_key = 'gcw2hzs1u'
 
+# Create URL from location key and today's date
 url = f"https://www.metoffice.gov.uk/weather/forecast/{location_key}#?date={str(today)}"
 
-response = requests.get(url)
+# Function to scrape forecast data from URL given parameter names
+def metoffice_param(url, paramdict):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    parse_today = soup.findAll('div', {"id":str(today)})
+    parse_tomorrow = soup.findAll('div', {'id':str(tomorrow)})
+    param = soup.findAll('tr', paramdict)
+    param_d = {}
+    for p in range(0, len(param)):
+        td = param[p].findAll('td', class_=True)
+        param_list = []
+        for i in range(0,len(td)):
+            param_list.append(param[p].findAll('td', class_=True)[i].text.strip())
+        param_d[p] = param_list
+    return param_d
 
-soup = BeautifulSoup(response.content, "html.parser")
+# Probability of precipitaion
+met_pop = metoffice_param(url, {'class':'step-pop'})
 
-pop = soup.findAll('tr', 'step-pop')
-
-pop_d = {}
-for p in range(0, len(pop)):
-    td = pop[p].findAll('td', class_=True)
-    pop_list = []
-    for i in range(0,len(td)):
-        pop_list.append(pop[p].findAll('td', class_=True)[i].text.strip())
-    pop_d[p] = pop_list
+# Feels like temperature
+met_flt = metoffice_param(url, {'class':'detailed-view','data-type':'temp'})
