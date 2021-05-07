22a7cf735f5207134bb4442a730e3d0040339b37
Quernon <36922275+Quernon@users.noreply.github.com>
2021-05-07 11:53:21 +0100

    Delivers POP & FLT.

--- bbc.py
+++ bbc.py
@@ -14,22 +14,22 @@ day_list = ['today', 'day1', 'day2', 'day3', 'day4', 'day5', 'day6', 'day7',
             'day8', 'day9', 'day10', 'day11', 'day12', 'day13']
 
 # Function to scrape forecast data from URL given parameter names
-def bbc_param(paramdict):
+def bbc_param(tag, paramdict, denominator):
     param_d = {}
     for day in day_list:
         # Create URL from location key and today's date
         url = f"https://www.bbc.co.uk/weather/{location_key}/{day}"
         response = requests.get(url)
         soup = BeautifulSoup(response.content, "html.parser")
-        param = soup.findAll('div', paramdict)
+        param = soup.findAll(tag, paramdict)
         param_list = []
         for p in range(0, len(param)):
-            param_list.append(param[p].text.strip().split('%')[0])
+            param_list.append(param[p].text.strip().split(denominator)[0])
         param_d[day] = param_list
     return param_d
 
 # Probability of precipitaion
-bbc_pop = bbc_param({'class':'wr-u-font-weight-500'})
+bbc_pop = bbc_param('div', {'class':'wr-u-font-weight-500'}, '%')
 
 # Feels like temperature
-bbc_flt = bbc_param({'class':'wr-value--temperature--c','data-type':'temp'})
+bbc_flt = bbc_param('span', {'class':'wr-value--temperature wr-temperature--time-slot'}, '°')
