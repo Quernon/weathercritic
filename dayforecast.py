005c78420a0a17c4d05c2b3e81b0d8ce3e307bac
Quernon <andrewsboulton@gmail.com>
2021-06-27 09:34:55 +0100

    Post ids changed to tomorrow

--- dayforecast.py
+++ dayforecast.py
@@ -1,10 +1,10 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from forecast import Forecast
 # from forecastwithaccuracy import ForecastWithAccuracy
 
 class DayForecast(object):
    def __init__(self):
-     self._id = datetime.today().strftime('%Y-%m-%d')
+     self._id = (datetime.today() + timedelta(days=1)).strftime('%Y-%m-%d')
      self.bbc = Forecast()
      self.met = Forecast()
      self.actual = Forecast()
